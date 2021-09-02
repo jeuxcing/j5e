9b3b878d2e0db1353ab8b678a1ad9ec3409dc244
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-09-02 21:57:11 +0200

    dict arduinos

--- j5e/network/__main__.py
+++ j5e/network/__main__.py
@@ -1,26 +1,24 @@
 
-import socket
+import subprocess as sp
+
+
+def list_arduinos():
+	arduinos = {}
+	# sys call to read kernel messages
+	dmesg = ["sudo", "dmesg"]
+	val = str(sp.run(dmesg, capture_output=True))
+	# split messages related to arduino
+	lines = val.split("\\n")
+	for idx, line in enumerate(lines):
+		if ("USB ACM device" in line):
+			port = line.split(":")[-2][1:]
+			serial = lines[idx-1].split(" ")[-1]
+			arduinos[serial] = port
+
+	print(arduinos)
+
 
 if __name__ == "__main__":
+	# value = sp.run(["sudo dmesg  |grep  -B1 \"USB ACM device\"  | awk '/SerialNumber/{ser=$6} /cdc_acm/{dev=substr($5,1,length($5)-1);if (ser!="")print ser\" /dev/\"dev}' |uniq"])
+	list_arduinos()
 
-	HOST = '172.16.180.10'
-	PORT = 6789
-
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.connect((HOST, PORT))
-	print 'Connexion vers ' + HOST + ':' + str(PORT) + ' reussie.'
-
-	message = 'Hello, world'
-	print 'Envoi de :' + message
-	n = client.send(message)
-	if (n != len(message)):
-    print 'Erreur envoi.'
-	else:
-	  print 'Envoi ok.'
-
-	print 'Reception...'
-	donnees = client.recv(1024)
-	print 'Recu :', donnees
-
-	print 'Deconnexion.'
-	client.close()
