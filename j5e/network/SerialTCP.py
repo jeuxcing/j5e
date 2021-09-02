eb2890e1386fdd9d983507936449ae01d0eeb4e4
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-09-02 22:32:07 +0200

    TCP/serial class

---
+++ j5e/network/SerialTCP.py
@@ -0,0 +1,36 @@
+import subprocess as sp
+
+
+class SerialTCP:
+
+    def __init__(self):
+        self.arduinos = {}
+
+    def list_arduinos(self):
+        # sys call to read kernel messages
+        dmesg = ["sudo", "dmesg"]
+        val = str(sp.run(dmesg, capture_output=True))
+        # split messages related to arduino
+        lines = val.split("\\n")
+        for idx, line in enumerate(lines):
+            if ("USB ACM device" in line):
+                port = line.split(":")[-2][1:]
+                serial = lines[idx-1].split(" ")[-1]
+                self.arduinos[serial] = port
+
+
+    def redirect(self, serial):
+        cmd = ["sudo", "socat", f"/dev/{self.arduinos[serial]},b115200,raw,echo=0", "tcp-listen:5555"]
+        val = sp.run(cmd, capture_output=True)
+        print(val)
+        print("ENDED")
+
+
+    def start(self):
+        self.list_arduinos()
+        if len(self.arduinos) == 0:
+            return
+
+        serial = next(iter(self.arduinos.keys()))
+        self.redirect(serial)
+
--- j5e/network/__main__.py
+++ j5e/network/__main__.py
@@ -1,24 +1,8 @@
 
-import subprocess as sp
+from j5e.network.SerialTCP import SerialTCP
 
-
-def list_arduinos():
-	arduinos = {}
-	# sys call to read kernel messages
-	dmesg = ["sudo", "dmesg"]
-	val = str(sp.run(dmesg, capture_output=True))
-	# split messages related to arduino
-	lines = val.split("\\n")
-	for idx, line in enumerate(lines):
-		if ("USB ACM device" in line):
-			port = line.split(":")[-2][1:]
-			serial = lines[idx-1].split(" ")[-1]
-			arduinos[serial] = port
-
-	print(arduinos)
 
 
 if __name__ == "__main__":
-	# value = sp.run(["sudo dmesg  |grep  -B1 \"USB ACM device\"  | awk '/SerialNumber/{ser=$6} /cdc_acm/{dev=substr($5,1,length($5)-1);if (ser!="")print ser\" /dev/\"dev}' |uniq"])
-	list_arduinos()
-
+	st = SerialTCP()
+	st.start()
